d9e297a541afd5b230dc530c542e34cfd41fdd4e
guilhermedmbasso <guilherme_m_basso@estudante.sesisenai.org.br>
2026-08-12 16:54:27 -0300

    Fix equality check in fatorial function
    sim

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const n = Number(prompt("Digite o número:"));
 
 function fatorial(n) {
-    if (n === 0 || n === 1) return 1;
+    if (n == 0 || n == 1) return 1;
     return n * fatorial(n - 1);
 }
 
